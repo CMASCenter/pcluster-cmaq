a3a82290ab5735b4298abb85da285fa628c4471d
lizadams <lizadams@email.unc.edu>
2022-04-14 19:25:02 +0000

    Update

--- qa_scripts/time_vs_cpu_two_axis_c59xlarge.r
+++ qa_scripts/time_vs_cpu_two_axis_c59xlarge.r
@@ -1,24 +1,24 @@
 # Script author: Liz Adams
 # Affiliation: UNC CMAS Center 
 
-csv_data<-read.csv("/shared/pcluster-cmaq/docs/user_guide_pcluster/timing_c5n18xlarge.csv",sep="\t", skip =0, header = TRUE, comment.char = "",check.names = FALSE, quote="", )
+csv_data<-read.csv("/shared/pcluster-cmaq/docs/user_guide_pcluster/qa/timing_c5n9xlarge.csv",sep="\t", skip =0, header = TRUE, comment.char = "",check.names = FALSE, quote="", )
 print(csv_data)
 
 # ------------- Do not change below unless modifying for a different workflow ---------------------
 
 
 # plot data
-   png(file = paste('c5n18xlarge','_','Time','_','Cost','_','CPUs','.png',sep=''), width = 1024, height = 768, bg='white')
+   png(file = paste('c5n9xlarge','_','Time','_','Cost','_','CPUs','.png',sep=''), width = 1024, height = 768, bg='white')
    x <- csv_data$CPUs
    y1 <- csv_data$TotalTime
    y2 <- csv_data$OnDemandCost
-   labels <- csv_data$NodesbyCPU
+   labels <- csv_data$NodesxCPU
 
 # Draw first plot using axis y1
 # sets the bottom, left, top and right margins respectively of the plot region in number of lines of text, + 0.2 gives extra room on the right margin for second legend.
 par(mar = c(7, 5, 5, 4) + 0.2)
 
-plot(x, y1, pch = 13, col = 2, xlab = "CPUs", ylab = "2 Day Total Time (sec)", main = "CMAQv533 Benchmark 2 Day Run Time (seconds) and OnDemand Cost vs CPUs on Parallel Cluster uing c5n.18xlarge Compute Nodes")
+plot(x, y1, pch = 13, col = 2, xlab = "CPUs", ylab = "2 Day Total Time (sec)", main = "CMAQv533 Benchmark 2 Day Run Time (seconds) and OnDemand Cost vs CPUs on Parallel Cluster uing c5n.9xlarge Compute Nodes")
 
 text(x, y1, labels, cex=0.65, pos=3,col="black")
 
