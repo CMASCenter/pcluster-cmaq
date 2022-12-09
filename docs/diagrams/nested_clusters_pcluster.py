d7e2b298508d1e467ee9205cf849c452cf69679d
lizadams <lizadams@email.unc.edu>
2022-12-09 21:15:04 +0000

    update

--- docs/diagrams/nested_clusters_pcluster.py
+++ docs/diagrams/nested_clusters_pcluster.py
@@ -4,29 +4,36 @@ from diagrams.aws.database import Redshift
 from diagrams.aws.integration import SQS
 from diagrams.aws.storage import S3, EBS, FSx
 from diagrams.aws.devtools import CLI
+from diagrams.azure.storage import StorageSyncServices
 
-with Diagram("AWS Minimum Viable Product", show=False):
+with Diagram("AWS Parallel Cluster", show=False):
     source = CLI("AWS CLI v3.0")
 
     with Cluster("Parallel Cluster"):
 
         queue = SQS("SLURM")
         
-        volume1 = EBS("Software/EBS Vol")
-
         with Cluster("Head Node"):
-            head = [EC2("c5n.xlarge")]
+            head = [EC2("c6a.xlarge")]
 
-        with Cluster("C5n.18xlarge - 36CPU/compute node"):
-            workers = [EC2SpotInstance("node1-spot"),
-                       EC2SpotInstance("node2-spot"),
-                        EC2SpotInstance("node3-spot"),
-                        EC2SpotInstance("node4-spot"),
-                        EC2SpotInstance("node5-spot")]
-        volume2 = FSx("I/O - 1.2TB Lustre Vol")
+        with Cluster("hpc6a.48xlarge - 96 CPU/compute node, Scalable by SLURM request"):
+            workers = [EC2("hpc6a.48xlarge"),
+                        EC2("hpc6a.48xlarge"),
+                        EC2("hpc6a.48xlarge")]
+        with Cluster("lustre"):
+            volume2 = [FSx("I/O - 1.2TB Lustre Vol")]
 
+        with Cluster("Shared"):
+            sched = [
+                     EC2("c6a.xlarge"),
+                     EBS("Software/EBS Vol")]
+        mnt = StorageSyncServices("Mount Point")
 
     store = S3("S3 Bucket")
 
-    source >> head >> volume1 >> queue >> workers
-    workers >> volume2 >> store
+    source >> head >> queue >> workers
+    workers >> mnt
+    mnt >> volume2 >> store
+    mnt >> sched >> store
+
+
