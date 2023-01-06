2585ad857e4099c85de89d647b260f55245b022f
lizadams <lizadams@email.unc.edu>
2023-01-06 16:39:10 -0500

    Update HPC6a Scaling plots

---
+++ qa_scripts/CMAQ_main_log_timing_analysis.pcluster.shared.r
@@ -0,0 +1,104 @@
+# Script modified to work with all processors
+# Script author: Jesse Bash
+# Affiliation: US EPA Office of Research and Development
+# This script assumes that the log files are located in ./CCTM/scripts as output by the CMAQ run script 
+# These are the single output files, not the CTM_LOG files found in the $OUTDIR/LOGS directory
+library(RColorBrewer)
+library(stringr)
+
+sens.dir  <- '/proj/ie/proj/CMAS/CMAQ/pcluster-cmaq/run_scripts/hpc6a/logs/'
+base.dir  <- '/proj/ie/proj/CMAS/CMAQ/pcluster-cmaq/run_scripts/hpc6a/logs/'
+#files   <- dir(sens.dir, pattern ='CTM_LOG_000.v533_gcc_2016_CONUS_480_24x20pe_lustre3_codemod_pin_precision_20151222')
+files   <- dir(sens.dir, pattern ='run_cctmv5.3.3_Bench_2016_12US2.hpc6a.48xlarge.96.1x96.12x8pe.2day.pcluster.pin.shared.log')
+#b.files <- c('run_cctmv5.3.3_Bench_2016_12US2.480.24x20pe.2day.cyclecloud.lustre3.codemod.pin.log')
+b.files <- c('run_cctmv5.3.3_Bench_2016_12US2.hpc6a.48xlarge.288.3x96.16x18pe.2day.pcluster.pin.shared.codemod.log')
+#b.files <- c('CTM_LOG_000.v533_gcc_2016_CONUS_480_24x20pe_lustre3_codemod_pin_precision_20151222')
+Compilers <- c('gcc')
+# name of the base case timing. I am using the current master branch from the CMAQ_Dev repository.
+# The project directory name is used for the sensitivity case. 
+base.name <- c('288pe')
+#base.name <- c('data_pin')
+sens.name <- c('96pe')
+
+# Simulation parameters
+
+
+
+
+# ------------- Do not change below unless modifying for a different workflow ---------------------
+# compilers being considered
+#Compilers <- c('intel','gcc','pgi')
+#Compilers <- c('intel')
+# parse directory and file information
+#sens.name <- strsplit(files,'/')
+#n.lev     <- length(sens.name[[1]])
+#sens.name <- sens.name[[1]][n.lev]
+all.names <- NULL
+for(i in 1:length(files)){
+   all.names <- append(all.names,sens.name)
+}
+files <- paste(sens.dir,files,sep="")
+for(i in 1:length(files)){
+   all.names <- append(all.names,base.name)
+}
+files <- append(files,paste(base.dir,b.files,sep=''))
+for( comp in Compilers) {
+   bar.data <- NULL
+   b.names <- NULL
+   for(i in 1:length(files)){
+# ignore debug simulations and get compiler information
+#      if(i%in%grep('debug',files)==F & i%in%grep(comp,files)){
+         file <- files[i]
+         b.names <- append(b.names,paste(all.names[i]))
+         data.in  <- scan(file,what='character',sep='\n')
+# get timing info
+         Timing <-  as.numeric(substr(data.in[grep('completed...',data.in)],36,42))
+
+        # master_timing <- as.numeric(substr(data.in[grep('Processing completed...',data.in)],36,42))
+         dataout_timing <- as.numeric(substr(data.in[grep('Data Output completed...',data.in)],36,45))
+         Process <- substr(data.in[grep('completed...',data.in)],12,22)
+        # Timing_sum <-sum(master_timing)
+        #dataio_sum <-sum(dataout_timing)
+         
+         Timing_direct <-  as.numeric(substr(data.in[grep('Total Time =',data.in)],18,26))
+        # Timing_missing <- Timing_direct - Timing_sum 
+         
+         n.proc <- unique(Process)
+         n.proc <- n.proc[grep('Proc',n.proc,invert=T)]
+ 
+
+	       # Aggregate data
+         tmp.data <- NULL
+         for(i in n.proc){
+           valid <- which(i==Process)
+           tmp.data <- append(tmp.data,sum(Timing[valid]))
+         }
+         Timing_sum <- sum(tmp.data)
+         Timing_missing <- Timing_direct - Timing_sum
+          
+         tmp.data <- append(tmp.data,Timing_missing)
+         bar.data <- cbind(bar.data,tmp.data)
+   #   }
+   }
+
+   n.proc.plot <- append(n.proc, "OTHER")
+   
+   #remove all leading whitespace
+   n.proc.plot <- str_trim(n.proc.plot, "left")
+   
+   # plot data
+   my.colors <- brewer.pal(12, "Paired")
+   #my.colors <- terrain.colors(length(n.proc))
+   xmax <- dim(bar.data)[2]*1.5
+   png(file = paste('hpc6a_shared_35GB_1+3nodes_',comp,'_all',sens.name,'_',base.name,'.png',sep=''), width = 1024, height = 768, bg='white')
+  # png(file = paste(comp,'_',sens.name,'.png',sep=''), width = 1024, height = 768, bg='white')
+   barplot(bar.data, main = 'Process Timing on /shared using 1+3 nodes with 96 cpus/node on HPC6a with pinning',names.arg = b.names,ylab='seconds', col = my.colors, legend = n.proc.plot, xlim = c(0.,xmax),ylim = c(0.,6000.))
+   box()
+   dev.off()
+ 
+  totals <- apply(bar.data,c(2),sum)
+# print total runtime data to the screen
+   for(i in 1:length(b.names)){
+     print(paste('Run time for', b.names[i], ':',totals[i],'seconds'))
+   }
+}
