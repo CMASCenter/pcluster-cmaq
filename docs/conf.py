52e6cf337fdc5980480d8f9f52e62c593b8ac766
lizadams <lizadams@email.unc.edu>
2023-03-15 14:53:39 +0000

    update CMAQ Logo

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath('_ext'))
 project = 'pcluster-cmaq'
 copyright = '2022, CMAS Center'
 author = 'Liz Adams'
-html_logo = 'CMAQLogo.jpg'
+html_logo = 'CMAQLogo.png'
 github_url = 'https://github.com/CMASCenter/pcluster-cmaq'
 
 
