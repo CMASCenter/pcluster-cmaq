ff3b4336a96f6a7ea56937946cdd2717d4df7e63
lizadams <lizadams@email.unc.edu>
2023-07-14 15:08:50 +0000

    update

---
+++ qa_scripts/time_vs_cpu_ggplot_c6a.48xlarge.r
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(patchwork) # To display 2 charts together
+
+# Script author: Liz Adams
+# Affiliation: UNC CMAS Center 
+# example from http://monashbioinformaticsplatform.github.io/2015-11-30-intro-r/ggplot.html
+# 2nd example from https://r-graph-gallery.com/line-chart-dual-Y-axis-ggplot2.html
+
+png(file = paste('c6a.48xlarge','_','Time','_','CPUs','.png',sep=''), width = 1024, height = 768, bg='white')
+csv_data<- read.csv("/shared/pcluster-cmaq/docs/user_guide_pcluster/post/qa/timing_c6a.48xlarge.tsv",sep="\t", skip =0, header = TRUE, comment.char = "",check.names = FALSE, quote="", )
+print(csv_data)
+p1 <- ggplot(csv_data, aes(y=TotalTime, x=CPUs, color=COLROW, shape=InputData, size = 3 )) +
+    geom_point() + ggtitle("2 Day Benchmark Total Time versus CPUs") + scale_y_continuous(name = "Total Time (seconds)")
+
+
+p2 <- ggplot(csv_data, aes(y=OnDemandCost, x=CPUs, color=COLROW,  shape=InputData, size = 3 )) +
+    geom_point() + ggtitle("2 Day Benchmark On Demand Cost versus CPUs") + scale_y_continuous(name = "On Demand Cost ($)")
+
+# Display both charts side by side thanks to the patchwork package
+p1 + p2
