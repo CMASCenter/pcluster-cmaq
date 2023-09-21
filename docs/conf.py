89666a177b124f606fa95f5aa06bc90612e4cd73
lizadams <lizadams@email.unc.edu>
2023-09-21 16:02:39 -0400

    update

--- docs/conf.py
+++ docs/conf.py
@@ -55,6 +55,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'furo'
+last_updated
+html_last_updated_fmt = ''
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
